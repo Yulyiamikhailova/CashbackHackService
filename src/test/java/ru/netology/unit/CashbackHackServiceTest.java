cb82a5d1202983ffda6b406929404cbe6642150b
“Yulia” <Yulyiamikhailova@gmail.com>
2022-07-25 21:23:49 +0300

    JUnit

---
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -0,0 +1,55 @@
+package ru.netology.unit;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void testRemainIf1000() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(0);
+        int expected = 1000;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemainIf0() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1000);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemainIf500() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(500);
+        int expected = 500;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemainIf1700() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1700);
+        int expected = 300;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemainIf2000() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(2000);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+
+}
